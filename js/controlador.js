f9dec2a71372b58ea5c2e8b6fda892fab151fdb1
Daniel Rivera <daniel.r9712@gmail.com>
2021-07-21 18:16:43 -0600

    Mejoras de elementos javascript
    Añadiendo validaciones, mejor definición de los elementos y generar código de seguridad (CCV) automáticamente al ingresar un solo dígito

--- js/controlador.js
+++ js/controlador.js
@@ -16,15 +16,96 @@
 --------------------------------------------------
   GRACIAS POR DESCARGAR ESTE PROYECTO :) ♥️
 --------------------------------------------------
+
+
+
+*******************************************************************************
+>>> NUEVA ACTUALIZACION <<<
+-> VALIDACION DE INPUT INGRESO DE DATOS
+-> GENERAR AUTOMATICAMENTE CODIGO DE SEGURIDAD AL INGRESAR UN SOLO DIGITO
+-> MENSAJES DE ALERTAS PERSONALIZADAS
+-> BLOQUEO TOTAL DE CARACTERES NO SOLICITADOS
+*******************************************************************************
+
 */
 
-// VARIABLES
+// VARIABLES GLOBALES
 const TarjetaClientes = document.querySelector('#tarjeta'), // TODO TARJETA CONTENEDOR
 IngresoDatos = document.querySelector('#datoscliente'), // TODO DATOS CLIENTES FORMULARIO
 NumeroUnicoTarjeta = document.querySelector('#tarjeta .numeros'), // NUMERO UNICO
 TitularTarjeta = document.querySelector('#tarjeta .nombre'), // TITULAR
 MiembroDesdeAnio = document.querySelector('#tarjeta .miembro'), // MIEMBRO {AÑO}
 CodigoSeguridad = document.querySelector('#tarjeta .codigo'); // CCV -> CODIGO DE SEGURIDAD
+
+// VALIDACION DE FORMULARIO INGRESO DE DATOS
+function ValidarTarjeta(){
+	// VARIABLES GENERALES DE VALIDACION
+	// TITULAR DE TARJETA
+	var TitularUnicoTarjeta = document.getElementById("nombrecliente").value; 
+	// NUMERO UNICO DE TARJETA
+	var NumeroTarjetaUnico = document.getElementById("numerocliente").value;
+	// CODIGO DE SEGURIDAD UNICO DE TARJETA -> CCV
+	var CodigoUnicoTarjeta = document.getElementById("codigocliente").value;
+	// MIEMBRO DESDE -> ULTIMOS DOS DÍGITOS
+	var MiembroTarjeta = document.getElementById("miembrocliente").value;
+	/*
+		--> VALIDACION ESTRICTAMENTE CAMPOS VACIOS
+	*/
+	// TITULAR DE TARJETA
+	if(TitularUnicoTarjeta==""){
+		AlertaUsuarioMostrar("Error","Lo sentimos, debe ingresar un titular único de tarjeta","error");
+		return false;
+	// NUMERO DE TARJETA
+	}else if(NumeroTarjetaUnico==""){
+		AlertaUsuarioMostrar("Error","Lo sentimos, debe ingresar un número único de tarjeta","error");
+		return false;
+	// CODIGO DE SEGURIDAD DE TARJETA
+	}else if(CodigoUnicoTarjeta==""){
+		AlertaUsuarioMostrar("Error","Lo sentimos, debe ingresar un código de seguridad único de tarjeta","error");
+		return false;
+	// AÑO DE AFILIACION TITULAR DE TARJETA
+	}else if(MiembroTarjeta==""){
+		AlertaUsuarioMostrar("Error","Lo sentimos, debe ingresar los últimos dos dígitos (año) de asociación","error");
+		return false;
+	}else{
+		// SI CUMPLE CON TODOS LOS REQUISITOS -> IMPRIMIR COMPROBANTE
+		window.print();
+	}
+}
+
+
+/*
+	--> VALIDAR CUMPLIMIENTO NUMERO MINIMO DE CARACTERES
+				CODIGO DE SEGURIDAD, NUMERO DE TARJETA Y FECHA DE ASOCIACION
+*/
+
+// NUMERO UNICO DE TARJETA
+function ValidarNumeroTarjeta(valor) {
+  if (valor.value.length < 16) {
+    AlertaUsuarioMostrar("Error","Lo sentimos, debe ingresar los 15 dígitos de la tarjeta","error");
+    return false;
+  }
+}
+
+// CODIGO DE SEGURIDAD UNICO DE TARJETA
+function ValidarCodigoTarjetaUnico(valorcodigo){
+	/*
+		ESTE CODIGO SE GENERARA DE MANERA AUTOMATICA
+		EL USUARIO UNICAMENTE DEBE INGRESAR UN NUMERO CUALQUIERA
+	*/
+	if(valorcodigo.value.length < 5){
+		// CALCULO DE CODIGO DE SEGURIDAD AUTOMATICO
+		var Minimo=1000; var Maximo=9999; 
+		var Clave =Math.floor(Math.random() * (+Maximo - +Minimo)) + +Minimo;
+		CodigoSeguridad.textContent = Clave; // IMPRIME EN TARJETA
+		document.getElementById('codigocliente').value = Clave; // IMPRIME EN FORMULARIO
+	}
+}
+
+/*
+	--> CONTROLADOR DINAMICO
+			DATOS GENERALES DE TARJETA {TODOS LOS DATOS}
+*/
 
 // NOMBRE DINAMICO TARJETAHABIENTE
 IngresoDatos.nombrecliente.addEventListener('keyup', (e) => {
@@ -56,7 +137,6 @@ IngresoDatos.codigocliente.addEventListener('keyup', () => {
 	IngresoDatos.codigocliente.value = IngresoDatos.codigocliente.value
 	.replace(/\s/g, '')
 	.replace(/\D/g, '');
-
 	CodigoSeguridad.textContent = IngresoDatos.codigocliente.value;
 });
 
@@ -71,3 +151,12 @@ IngresoDatos.miembrocliente.addEventListener('keyup', () => {
 		desde.textContent = 21; // MODIFICAR SEGUN AÑO EN CUESTION
 	}
 });
+
+// FUNCION PARA MOSTRAR ALERTAS A USUARIOS
+function AlertaUsuarioMostrar(titulo, descripcion, icono) {
+    Swal.fire(
+        titulo, // ENCABEZADO 
+        descripcion, // CUERPO
+        icono // ICONO DE ALERTA
+    );
+}
